cb11be87825799a9e40bbcdf211a0ffda1b3bd05
Minigrim0 <grimauflorent@gmail.com>
2024-03-15 10:06:09 +0100

    [Upd] run machine in main

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ use machine::Machine;
 use program::Program;
 
 fn main() {
-    let prog = Program::load("examples/binary/demo_xor.bin");
+    let prog = Program::load("examples/binary/challenge.bin");
     let mut machine = Machine::new();
     machine.load(prog);
+    if let Err(e) = machine.run() {
+        panic!("An error occured while running the machine: {}", e.to_string());
+    }
 }
